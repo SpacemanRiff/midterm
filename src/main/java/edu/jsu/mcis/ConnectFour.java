a87c7f8ff0a7e849374a80695cd59373f45d93bd
Scott Troutman <troutmansm@gmail.com>
2015-02-27 11:41:40 -0600

    All unit tests are written and passing.

--- src/main/java/edu/jsu/mcis/ConnectFour.java
+++ src/main/java/edu/jsu/mcis/ConnectFour.java
@@ -2,20 +2,51 @@ package edu.jsu.mcis;
 
 public class ConnectFour {
     public enum Location {EMPTY, RED, BLACK};
+    private Location[][] board;
+    private int currentTurn;
     
     public ConnectFour() {
-        
+        board = new Location[7][6];
+        currentTurn = 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                board[i][j] = Location.EMPTY;
+            }
+        }
     }
     
     public Location getTopOfColumn(int column) {
-
+        for(int i = board[column].length-1; i >= 0; i--){
+            if(board[column][i] != Location.EMPTY){
+                return board[column][i];
+            }
+        }
+        return Location.EMPTY;
     }
     
-    public int getHeightOfColumn(int column) {
-        
+    public int getHeightOfColumn(int column) {      
+        int height = 0;
+        for(int i = 0; i < board[column].length; i++){
+            if(board[column][i] != Location.EMPTY){
+                height++;
+            }
+        }
+        return height;
     }
     
     public void dropToken(int column) {
-        
+        try{
+            switch(currentTurn%2){
+                case 0:
+                    board[column][getHeightOfColumn(column)] = Location.RED;
+                    break;
+                case 1:
+                    board[column][getHeightOfColumn(column)] = Location.BLACK;
+                    break;                
+            }
+            currentTurn++;  
+        }catch(ArrayIndexOutOfBoundsException ex){
+            throw new ColumnFullException("The column selected, [" + column + "], was full.");
+        }
     }
 }
--- src/test/java/edu/jsu/mcis/ConnectFourTest.java
+++ src/test/java/edu/jsu/mcis/ConnectFourTest.java
@@ -26,9 +26,27 @@ public class ConnectFourTest{
     
     @Test(expected=ColumnFullException.class)
     public void testDroppingOnAFullColumnShouldThrowException() {
-        
-    }
-    
+        ConnectFour c = new ConnectFour();
+        c.dropToken(2);
+        assertEquals(ConnectFour.Location.RED, c.getTopOfColumn(2));
+        assertEquals(1, c.getHeightOfColumn(2));
+        c.dropToken(2);
+        assertEquals(ConnectFour.Location.BLACK, c.getTopOfColumn(2));
+        assertEquals(2, c.getHeightOfColumn(2));
+        c.dropToken(2);
+        assertEquals(ConnectFour.Location.RED, c.getTopOfColumn(2));
+        assertEquals(3, c.getHeightOfColumn(2));
+        c.dropToken(2);
+        assertEquals(ConnectFour.Location.BLACK, c.getTopOfColumn(2));
+        assertEquals(4, c.getHeightOfColumn(2));    
+        c.dropToken(2);
+        assertEquals(ConnectFour.Location.RED, c.getTopOfColumn(2));
+        assertEquals(5, c.getHeightOfColumn(2));
+        c.dropToken(2);  
+        assertEquals(ConnectFour.Location.BLACK, c.getTopOfColumn(2));
+        assertEquals(6, c.getHeightOfColumn(2)); 
+        c.dropToken(2);
+    }    
 }
 
 
